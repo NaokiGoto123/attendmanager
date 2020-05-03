2fd275bb3f477527ad2c2018b626c1dbd8e88652
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-04 04:02:23 +0900

    feat(card): create event card

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { EventCardComponent } from './event-card/event-card.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 
 @NgModule({
   declarations: [EventCardComponent, SidenavComponent],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    MatCardModule,
+    MatButtonModule,
   ]
 })
 export class SharedModule { }
