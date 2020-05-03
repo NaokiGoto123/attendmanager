a6381ce003333ea46285dfd1d88da8bffdb1372b
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-03 18:01:41 +0900

    feat(form): add form to welcome component

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [WelcomeComponent],
   imports: [
     CommonModule,
-    WelcomeRoutingModule
+    WelcomeRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
   ]
 })
 export class WelcomeModule { }
--- src/app/welcome/welcome/welcome.component.ts
+++ src/app/welcome/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+
+  form = this.fb.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  submit(){
+    console.log(this.form.value);
   }
 
 }
