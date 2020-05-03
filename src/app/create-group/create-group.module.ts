a2a43345561c7bbccb5efd581c17b2338fcc03c9
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-04 03:33:50 +0900

    feat(form): create form using angular material

--- src/app/create-group/create-group.module.ts
+++ src/app/create-group/create-group.module.ts
@@ -3,13 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { CreateGroupRoutingModule } from './create-group-routing.module';
 import { CreateGroupComponent } from './create-group/create-group.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 @NgModule({
   declarations: [CreateGroupComponent],
   imports: [
     CommonModule,
-    CreateGroupRoutingModule
+    CreateGroupRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
   ]
 })
 export class CreateGroupModule { }
--- src/app/create-group/create-group/create-group.component.ts
+++ src/app/create-group/create-group/create-group.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-create-group',
   templateUrl: './create-group.component.html',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateGroupComponent implements OnInit {
 
-  constructor() { }
+  form = this.fb.group({
+    name: ['', [Validators.required]],
+    description: [''],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  submit(){
+    console.log(this.form.value);
+  }
+
+  openSnackBar() {
+    this.snackBar.open('Successfully created!', '', {
+      duration: 3000,
+    });
   }
 
 }
