4e146d618013053748d1f7372a3607b6e072969e
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-06 00:25:34 +0900

    feat(toolbar): create toolbar with add icon

--- src/app/groups/groups.module.ts
+++ src/app/groups/groups.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { GroupsRoutingModule } from './groups-routing.module';
 import { GroupsComponent } from './groups/groups.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 
 @NgModule({
   declarations: [GroupsComponent],
   imports: [
     CommonModule,
-    GroupsRoutingModule
+    GroupsRoutingModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
   ]
 })
 export class GroupsModule { }
