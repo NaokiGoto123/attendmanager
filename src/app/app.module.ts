ace19ab1ef0526a57d4b2cc8f1861c7438dc4c68
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-07 03:00:40 +0900

    feat(mat badge): add mat badge to notification nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatListModule } from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatBadgeModule} from '@angular/material/badge';
 
 
 
@@ -34,6 +35,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatButtonModule,
     MatIconModule,
     SharedModule,
+    MatBadgeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
