45cc7e7c4d395474753110964bc8132076d9dbea
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-06 19:16:22 +0900

    fix(style): place the spinner at the center

--- src/app/spinner/spinner.module.ts
+++ src/app/spinner/spinner.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { SpinnerRoutingModule } from './spinner-routing.module';
 import { SpinnerComponent } from './spinner/spinner.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 @NgModule({
   declarations: [SpinnerComponent],
   imports: [
     CommonModule,
-    SpinnerRoutingModule
+    SpinnerRoutingModule,
+    MatProgressSpinnerModule
   ]
 })
 export class SpinnerModule { }
