0979c12c2df77d32cc32ac9062010f69c3e501e6
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-07 18:39:17 +0900

    feat(delete): delete notfound module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CreateEventComponent } from './create-event/create-event/create-event.component';
-import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
   {
@@ -15,11 +13,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./shell/shell.module').then((m) => m.ShellModule),
   },
-  {
-    path: '**', // this path should be the last.
-    pathMatch: 'full',
-    component: NotfoundComponent,
-  },
+  // {
+  //   path: '**', // this path should be the last.
+  //   pathMatch: 'full',
+  //   component: NotfoundComponent,
+  // },
 ];
 
 @NgModule({
