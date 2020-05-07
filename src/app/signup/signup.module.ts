4bf94e68b613a638e15df66af4bc34abcab8e315
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-07 16:41:25 +0900

    feat(signup): add signup form

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -3,13 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup/signup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 @NgModule({
   declarations: [SignupComponent],
   imports: [
     CommonModule,
-    SignupRoutingModule
+    SignupRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
   ]
 })
 export class SignupModule { }
--- src/app/signup/signup/signup.component.ts
+++ src/app/signup/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+
+  form = this.fb.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  submit(){
+    console.log(this.form.value);
+  }
+
+  openSnackBar() {
+    this.snackBar.open('Signed in', '', {
+      duration: 3000,
+    });
   }
 
 }
