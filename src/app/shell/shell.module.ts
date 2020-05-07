2b9f2b96fc9370674519f0f49964cf5b5601f745
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-07 22:49:11 +0900

    feat(shell): add toolbar and sidenav to shell component

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -3,9 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { ShellRoutingModule } from './shell-routing.module';
 import { ShellComponent } from './shell/shell.component';
+import { HeaderComponent } from './header/header.component';
+import { SharedModule } from './shared/shared.module';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
-  declarations: [ShellComponent],
-  imports: [CommonModule, ShellRoutingModule],
+  declarations: [ShellComponent, HeaderComponent],
+  imports: [
+    CommonModule,
+    ShellRoutingModule,
+    SharedModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule,
+  ],
 })
 export class ShellModule {}
