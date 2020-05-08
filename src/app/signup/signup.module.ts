6606789d27627bf60093408550e3456d3b003fc5
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-09 01:24:33 +0900

    feat(link): add link to welcome module

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -4,12 +4,12 @@ import { CommonModule } from '@angular/common';
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup/signup.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [SignupComponent],
@@ -23,6 +23,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatIconModule,
     MatButtonModule,
     MatSnackBarModule,
-  ]
+    MatToolbarModule,
+  ],
 })
-export class SignupModule { }
+export class SignupModule {}
