29c823e053f2507330898e5cdab660bcf4c98a5a
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-10 01:49:16 +0900

    feat(component.ts): define event

--- src/app/shell/search/search/search.component.ts
+++ src/app/shell/search/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from 'src/app/interfaces/event';
 
 @Component({
   selector: 'app-search',
@@ -7,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
   value = 'Look for what you want';
+
+  event: Event = {
+    eventid: '1',
+    title: 'Picnic',
+    desription: 'You wanna go for a walk this weekend? Join us asap!!',
+    limit: 9,
+    date: '12/03/2020',
+    time: '12:00',
+    location: 'Mountian',
+    groupid: 'abc123',
+    grouppicture: '/assets/images/example.jpg',
+  };
 
   constructor() {}
 
