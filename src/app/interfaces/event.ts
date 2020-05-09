e0654d3dedc7805d6f152bb4ebaffe1b6f4f2350
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-10 01:47:24 +0900

    feat(interfaces): change and add more properties

--- src/app/interfaces/event.ts
+++ src/app/interfaces/event.ts
@@ -1,8 +1,13 @@
+import { Group } from './group'; // なんでgroup.tsでEventを使う時にはインポートする必要がなかったのに、groupを使う時にはインポート四角てはいけなかったのか
+
 export interface Event {
+  eventid: string; // イベントID
   title: string; // イベントタイトル
   desription: string; // イベント詳細
   limit: number; // 参加人数制限
   date: string; // イベント日付
   time: string; // イベント時間
   location: string; // イベント場所
+  groupid: string; // イベントが作られたグループID
+  grouppicture: string; // イベントが作られたグループ写真
 }
--- src/app/interfaces/group.ts
+++ src/app/interfaces/group.ts
@@ -1,6 +1,10 @@
+import { Event } from './event';
+
 export interface Group {
+  groupid: string; // グループID
   name: string; // グループ名
   description: string; // グループ詳細
+  grouppicture: string; // グループ画像
   creater: string; // グループ作成者
   admin: string[]; // グループ管理者（達）
   members: string[]; // グループメンバー（達）
