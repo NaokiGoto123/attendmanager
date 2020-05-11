8fe7f61b5075e31dc2aed52c97aff9b5ee69af39
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-12 03:59:41 +0900

    fix(text): fix text

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -3,8 +3,8 @@ import { Event } from './event';
 
 export interface User {
   uid: string; // ユニークID
-  diplayname: string; // 表示名
+  displayname: string; // 表示名
   email: string; // メールアドレス
   groups: Group[]; // 参加しているグループ（複数）
-  Events: Event[]; // 参加しているイベント（複数）
+  events: Event[]; // 参加しているイベント（複数）
 }
