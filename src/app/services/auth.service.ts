23db371d437870e22d3e6aac4e1db02b12445eb7
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-12 04:00:00 +0900

    feat(auth): add google signin and signout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,63 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../interfaces/user';
+
+import { auth } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
+
+import { Observable, of, merge } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  user$: Observable<any>;
 
-  login() {}
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap((user) => {
+        if (user) {
+          this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        } else {
+          return null;
+        }
+      })
+    );
+  }
 
-  logout() {}
+  async googleSignin() {
+    const provider = new auth.GoogleAuthProvider();
+    const credential = await this.afAuth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
+  }
+
+  async signOut() {
+    await this.afAuth.signOut();
+    return this.router.navigate(['/welcome']);
+  }
+
+  private updateUserData(user) {
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(
+      `users/${user.uid}`
+    );
+
+    const data = {
+      uid: user.uid,
+      displayname: user.displayname,
+      email: user.email,
+      groups: user.groups,
+      events: user.events,
+    };
+
+    return userRef.set(data, { merge: true });
+  }
 }
