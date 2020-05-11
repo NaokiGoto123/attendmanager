eeeffeda3af74e9fccb890835edbd9bf7b01fc0d
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-11 17:31:46 +0900

    feat(mat input0: add search box to group module

--- src/app/shell/groups/groups.module.ts
+++ src/app/shell/groups/groups.module.ts
@@ -3,22 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { GroupsRoutingModule } from './groups-routing.module';
 import { GroupsComponent } from './groups/groups.component';
+import { SharedModule } from '../shared/shared.module';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatChipsModule } from '@angular/material/chips';
-import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [GroupsComponent],
   imports: [
     CommonModule,
     GroupsRoutingModule,
+    SharedModule,
     MatMenuModule,
     MatIconModule,
     MatToolbarModule,
     MatChipsModule,
-    SharedModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
   ],
 })
 export class GroupsModule {}
