fe3ffc96e781e0f17f8aa26f1a3733deae51c4f3
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-13 14:36:23 +0900

    feat(authguard): add authguard

--- src/app/shell/shell-routing.module.ts
+++ src/app/shell/shell-routing.module.ts
@@ -12,6 +12,8 @@ const routes: Routes = [
         path: 'events',
         loadChildren: () =>
           import('./events/events.module').then((m) => m.EventsModule),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'create-event',
@@ -19,11 +21,15 @@ const routes: Routes = [
           import('./create-event/create-event.module').then(
             (m) => m.CreateEventModule
           ),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'groups',
         loadChildren: () =>
           import('./groups/groups.module').then((m) => m.GroupsModule),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'groups/create-group',
@@ -31,6 +37,8 @@ const routes: Routes = [
           import('./create-group/create-group.module').then(
             (m) => m.CreateGroupModule
           ),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'search',
@@ -43,21 +51,29 @@ const routes: Routes = [
           import('./notifications/notifications.module').then(
             (m) => m.NotificationsModule
           ),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'account',
         loadChildren: () =>
           import('./account/account.module').then((m) => m.AccountModule),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'settings',
         loadChildren: () =>
           import('./settings/settings.module').then((m) => m.SettingsModule),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'shared',
         loadChildren: () =>
           import('./shared/shared.module').then((m) => m.SharedModule),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
     ],
   },
