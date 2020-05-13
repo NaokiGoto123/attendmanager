264e6ff00fd648d8f338b9f0e211f8a0bc90e271
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-13 13:41:33 +0900

    fix(typo): add return in line 39

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
     this.user$ = this.afAuth.authState.pipe(
       switchMap((user) => {
         if (user) {
-          this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
         } else {
           return of(null);
         }
