74172a47cb63e43bed710d5d14b0d393e9e4b284
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-13 14:35:21 +0900

    feat(pathmatch): add pathmatch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,19 @@ import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
   {
     path: 'welcome',
-    pathMatch: 'full',
     loadChildren: () =>
       import('./welcome/welcome.module').then((m) => m.WelcomeModule),
   },
   {
     path: 'welcome/signup',
-    pathMatch: 'full',
     loadChildren: () =>
       import('./signup/signup.module').then((m) => m.SignupModule),
   },
   {
     path: '',
+    pathMatch: 'full',
     loadChildren: () =>
       import('./shell/shell.module').then((m) => m.ShellModule),
-    canLoad: [AuthGuard],
-    canActivate: [AuthGuard],
   },
 ];
 
