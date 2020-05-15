618545eaa1414c0b153c36bc17a788a59c956d66
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-15 17:59:53 +0900

    feat(service): define another method

--- src/app/services/group.service.ts
+++ src/app/services/group.service.ts
@@ -2,22 +2,26 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Group } from '../interfaces/group';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class GroupService {
   constructor(private db: AngularFirestore, private snackbar: MatSnackBar) {}
 
-  createGroup(group: Group) {
+  async createGroup(group: Group) {
     const id = group.groupid;
+    await this.db.doc(`organizations/${id}`).set(group);
+    this.snackbar.open('Successfully created the group', null, {
+      duration: 2000,
+    });
+  }
+
+  getGroup(uid: string): Observable<Group[]> {
     return this.db
-      .doc(`organizations/${id}`)
-      .set(group)
-      .then(() => {
-        this.snackbar.open('Successfully created the group', null, {
-          duration: 2000,
-        });
-      });
+      .collection<Group>('organization', (ref) =>
+        ref.where('members', 'array-contains', 'uid')
+      )
+      .valueChanges();
   }
 }
