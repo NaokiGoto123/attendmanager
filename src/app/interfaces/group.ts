cc82b01f7cf6617beb2c80bc9a737f5a6c683b1d
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-15 17:58:45 +0900

    feat(interfaces): update group interface

--- src/app/interfaces/group.ts
+++ src/app/interfaces/group.ts
@@ -2,7 +2,8 @@ export interface Group {
   groupid: string; // グループID
   name: string; // グループ名
   description: string; // グループ詳細
-  grouppicture: string; // グループ画像
+  grouppicture: number; // グループ画像
+  createddate: Date;
   creater: string; // グループ作成者
   admin: string[]; // グループ管理者（達）
   members: string[]; // グループメンバー（達）
