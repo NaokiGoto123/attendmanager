3dd86c24bbbc258b807870cd4cfa467cf95acaa0
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-18 02:33:58 +0900

    feat(group service): add methods

--- src/app/services/group.service.ts
+++ src/app/services/group.service.ts
@@ -3,7 +3,6 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Group } from '../interfaces/group';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
@@ -12,17 +11,33 @@ export class GroupService {
 
   async createGroup(group: Group) {
     const id = group.groupid;
-    await this.db.doc(`organizations/${id}`).set(group);
-    this.snackbar.open('Successfully created the group', null, {
-      duration: 2000,
-    });
+    await this.db
+      .doc(`organizations/${id}`)
+      .set(group)
+      .then(() =>
+        this.snackbar.open('Successfully created the group', null, {
+          duration: 2000,
+        })
+      );
   }
 
-  getGroup(uid: string): Observable<Group[]> {
+  getMyGroup(uid: string): Observable<Group[]> {
     return this.db
       .collection<Group>(`organizations`, (ref) =>
         ref.where(`members`, 'array-contains', uid)
       )
       .valueChanges();
+  }
+
+  getAdminGroup(uid: string): Observable<Group[]> {
+    return this.db
+      .collection<Group>(`organizations`, (ref) =>
+        ref.where(`admin`, 'array-contains', uid)
+      )
+      .valueChanges();
+  }
+
+  getGroupinfo(groupid: string) {
+    return this.db.doc<Group>(`organizations/${groupid}`).valueChanges();
   }
 }
