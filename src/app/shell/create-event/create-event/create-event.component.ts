2e264e24b78005e721bcc4ab40a07d846dd1944e
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-18 02:34:14 +0900

    feat(mat select): add mat select

--- src/app/shell/create-event/create-event/create-event.component.ts
+++ src/app/shell/create-event/create-event/create-event.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { GroupService } from 'src/app/services/group.service';
+import { EventService } from 'src/app/services/event.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
+import { Group } from 'src/app/interfaces/group';
+import { map, tap } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-create-event',
@@ -9,6 +17,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class CreateEventComponent implements OnInit {
   form = this.fb.group({
+    groupid: ['', [Validators.required]],
     title: ['', [Validators.required]],
     description: [''],
     memberlimit: [0],
@@ -16,18 +25,35 @@ export class CreateEventComponent implements OnInit {
     time: ['00:00', [Validators.required]],
     location: ['', [Validators.required]],
   });
+  // tslint:disable-next-line: max-line-length
+  originalgroups$: Observable<Group[]> = this.groupService.getAdminGroup(
+    this.authService.uid
+  );
 
-  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {}
+  // tslint:disable-next-line: max-line-length
+  constructor(
+    private fb: FormBuilder,
+    private db: AngularFirestore,
+    private authService: AuthService,
+    private groupService: GroupService,
+    private eventService: EventService
+  ) {}
 
   ngOnInit(): void {}
 
   submit() {
-    console.log(this.form.value);
-  }
-
-  openSnackBar() {
-    this.snackBar.open('Successfully created the event!', '', {
-      duration: 3000,
+    // tslint:disable-next-line: max-line-length
+    // const grouppicturenumber: number = this.groupService.getGroupinfo(this.form.value.groupid).pipe(map(group => group.grouppicture = grouppicturenumber));
+    this.eventService.createEvent({
+      eventid: this.db.createId(),
+      title: this.form.value.title,
+      description: this.form.value.description,
+      memberlimit: this.form.value.memberlimit,
+      date: this.form.value.date,
+      time: this.form.value.time,
+      location: this.form.value.location,
+      groupid: this.form.value.groupid,
+      grouppicture: null,
     });
   }
 }
