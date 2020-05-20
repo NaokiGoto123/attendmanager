2d79512a6441eb37fcbdb847d48d250b17568411
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-20 18:23:19 +0900

    feat(event interface): update interface

--- src/app/interfaces/event.ts
+++ src/app/interfaces/event.ts
@@ -1,10 +1,9 @@
-import { Observable } from 'rxjs';
-
 export interface Event {
   eventid: string; // イベントID
   title: string; // イベントタイトル
   description: string; // イベント詳細
   memberlimit: number; // 参加人数制限
+  attendingmembers: string[]; // 参加中の人達
   date: string; // イベント日付
   time: string; // イベント時間
   location: string; // イベント場所
