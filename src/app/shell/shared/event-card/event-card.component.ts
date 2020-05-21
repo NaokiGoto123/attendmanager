971bed65b9846907a7f48e6330c7a755a25d0eca
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-22 03:39:48 +0900

    feat(event card): toggle attended automatically

--- src/app/shell/shared/event-card/event-card.component.ts
+++ src/app/shell/shared/event-card/event-card.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Event } from 'src/app/interfaces/event';
-import { Observable } from 'rxjs';
-
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-event-card',
   templateUrl: './event-card.component.html',
@@ -12,9 +11,17 @@ export class EventCardComponent implements OnInit {
 
   attended = false;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
+
+  // ngOnInit(): void {
+  //   if (this.authService.uid in this.event.attendingmembers) {
+  //     this.attended = true;
+  //   } else {
+  //     this.attended = false;
+  //   }
+  // }
 
   switchStatus() {
     this.attended = !this.attended;
