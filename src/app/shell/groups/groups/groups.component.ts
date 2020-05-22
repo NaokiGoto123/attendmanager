77eff7ac6c2b73c0a4c84103a32b5da12a96cc22
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-23 03:21:01 +0900

    feat(events, groups, search): update

--- src/app/shell/groups/groups/groups.component.ts
+++ src/app/shell/groups/groups/groups.component.ts
@@ -5,6 +5,7 @@ import { Group } from 'src/app/interfaces/group';
 import { AuthService } from 'src/app/services/auth.service';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-groups',
@@ -12,29 +13,30 @@ import { map, tap } from 'rxjs/operators';
   styleUrls: ['./groups.component.scss'],
 })
 export class GroupsComponent implements OnInit {
-  event: Event = {
-    eventid: '1',
-    title: 'Picnic',
-    description: 'You wanna go for a walk this weekend? Join us asap!!',
-    memberlimit: 9,
-    attendingmembers: [],
-    date: '12/03/2020',
-    time: '12:00',
-    location: 'Mountian',
-    groupid: 'abc123',
-    grouppicture: 6,
-  };
-
   value = 'Look for what you want';
 
   groups$: Observable<Group[]> = this.groupService.getMyGroup(
     this.authService.uid
   );
 
+  nodata: boolean;
+
+  events: Observable<Event[]> = this.eventService.getEvents(
+    this.authService.uid
+  );
+
   constructor(
     private groupService: GroupService,
-    private authService: AuthService
+    private authService: AuthService,
+    private eventService: EventService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.events === null) {
+      this.nodata = true;
+    } else {
+      this.nodata = false;
+    }
+    console.log(this.nodata);
+  }
 }
