1e149584fb972c6292750313392bbbe6af26aace
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-25 01:16:11 +0900

    feat(form guard): create form guard

--- src/app/shell/create-group/create-group-routing.module.ts
+++ src/app/shell/create-group/create-group-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateGroupComponent } from './create-group/create-group.component';
+import { FormGuard } from 'src/app/guards/form.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: CreateGroupComponent,
+    canDeactivate: [FormGuard],
   },
 ];
 
