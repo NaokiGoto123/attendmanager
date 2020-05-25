cfd2d77916b7fb0e74d831a3d8f2f0b3b07d4a8f
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-05-25 17:57:38 +0900

    feat(environemnt.prod): add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBHnh-u9rUwYdWbHVm4j9uVp58rdD5mloU',
+    authDomain: 'attendmanager-c8784.firebaseapp.com',
+    databaseURL: 'https://attendmanager-c8784.firebaseio.com',
+    projectId: 'attendmanager-c8784',
+    storageBucket: 'attendmanager-c8784.appspot.com',
+    messagingSenderId: '769641726969',
+    appId: '1:769641726969:web:58bf3148cd63dde1f37328',
+    measurementId: 'G-J82MBM5ZWH',
+  },
 };
