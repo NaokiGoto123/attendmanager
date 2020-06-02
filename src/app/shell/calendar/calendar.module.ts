390c95939061bbef6c913fbdbc1ca0535f8552b8
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-06-03 00:58:30 +0900

    feat(calendar): implement full calendar

--- src/app/shell/calendar/calendar.module.ts
+++ src/app/shell/calendar/calendar.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarComponent } from './calendar/calendar.component';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FullCalendarModule } from '@fullcalendar/angular'; // for FullCalendar!
 @NgModule({
   declarations: [CalendarComponent],
-  imports: [CommonModule, CalendarRoutingModule],
+  imports: [
+    CommonModule,
+    CalendarRoutingModule,
+    BrowserModule,
+    FullCalendarModule,
+  ],
 })
 export class CalendarModule {}
