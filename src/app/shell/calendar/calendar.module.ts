431f45740d6e8258a78f3d7323c17be577482362
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-06-04 01:43:46 +0900

    feat(calendar): add funcdtions

--- src/app/shell/calendar/calendar.module.ts
+++ src/app/shell/calendar/calendar.module.ts
@@ -4,9 +4,19 @@ import { CommonModule } from '@angular/common';
 import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarComponent } from './calendar/calendar.component';
 import { FullCalendarModule } from '@fullcalendar/angular'; // for FullCalendar!
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [CalendarComponent],
-  imports: [CommonModule, CalendarRoutingModule, FullCalendarModule],
+  imports: [
+    CommonModule,
+    CalendarRoutingModule,
+    FullCalendarModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CalendarModule {}
--- src/app/shell/calendar/calendar/calendar.component.ts
+++ src/app/shell/calendar/calendar/calendar.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import { EventService } from 'src/app/services/event.service';
+import { GroupService } from 'src/app/services/group.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable, combineLatest } from 'rxjs';
+import { Event } from 'src/app/interfaces/event';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
@@ -8,7 +15,43 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 export class CalendarComponent implements OnInit {
   calendarPlugins = [dayGridPlugin];
 
-  constructor() {}
+  eventNames$: string[];
 
-  ngOnInit(): void {}
+  eventDates$: Date[];
+
+  constructor(
+    private authService: AuthService,
+    private eventService: EventService,
+    private groupService: GroupService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.eventService
+      .getEvents(this.authService.uid)
+      .subscribe((events: Event[]) => {
+        events.forEach((event) => {
+          const result: string[] = [];
+          result.push(event.title);
+          console.log(result);
+          this.eventNames$ = result;
+        });
+      });
+    this.eventService
+      .getEvents(this.authService.uid)
+      .subscribe((events: Event[]) => {
+        events.forEach((event) => {
+          const result: Date[] = [];
+          result.push(event.date.toDate());
+          console.log(result);
+          this.eventDates$ = result;
+        });
+      });
+    console.log(this.eventNames$);
+    console.log(this.eventDates$);
+  }
+
+  navigateBack() {
+    this.location.back();
+  }
 }
