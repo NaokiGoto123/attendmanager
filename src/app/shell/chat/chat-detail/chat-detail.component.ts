9a3b6a96e50f150037e8588ad9822fbde6900436
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-06-11 02:07:22 +0900

    feat(chat detail): create component

---
+++ src/app/shell/chat/chat-detail/chat-detail.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/services/chat.service';
+import { ActivatedRoute } from '@angular/router';
+import { ChatRoom } from 'src/app/interfaces/chat-room';
+import { Message } from 'src/app/interfaces/message';
+import { AuthService } from 'src/app/services/auth.service';
+import { firestore } from 'firebase';
+
+@Component({
+  selector: 'app-chat-detail',
+  templateUrl: './chat-detail.component.html',
+  styleUrls: ['./chat-detail.component.scss'],
+})
+export class ChatDetailComponent implements OnInit {
+  loading = true;
+
+  noMessage: boolean;
+
+  chatRoomId: string;
+
+  uid: string;
+
+  photoURL: string;
+
+  message = '';
+
+  name: string;
+
+  messages: Message[];
+
+  groupid: string;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private chatService: ChatService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.uid = this.authService.uid;
+    this.photoURL = this.authService.photoURL;
+    this.activatedRoute.queryParamMap.subscribe((params) => {
+      const chatRoomId = params.get('id');
+      this.chatRoomId = chatRoomId;
+      this.chatService
+        .getChatRoom(chatRoomId)
+        .subscribe((ChatRoom: ChatRoom) => {
+          this.name = ChatRoom.name;
+          this.groupid = ChatRoom.groupid;
+          this.messages = ChatRoom.messages;
+          if (this.messages.length) {
+            this.noMessage = false;
+          } else {
+            this.noMessage = true;
+          }
+        });
+    });
+    this.loading = false;
+  }
+
+  send() {
+    this.chatService.sendMessage(
+      {
+        ownerId: this.uid,
+        content: this.message,
+        sentAt: firestore.Timestamp.now(),
+      },
+      this.chatRoomId
+    );
+    this.message = null;
+  }
+}
