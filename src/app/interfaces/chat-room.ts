826dbb4ea6778224540b6407564d63ee5b3c495b
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-06-11 02:07:02 +0900

    feat(interfaces): update

--- src/app/interfaces/chat-room.ts
+++ src/app/interfaces/chat-room.ts
@@ -2,6 +2,7 @@ import { Message } from './message';
 
 export interface ChatRoom {
   id: string;
+  name: string;
   groupid: string;
   members: string[];
   messages: Message[];
--- src/app/interfaces/message.ts
+++ src/app/interfaces/message.ts
@@ -1,4 +1,7 @@
+import { firestore } from 'firebase';
+
 export interface Message {
   ownerId: string;
   content: string;
+  sentAt: firestore.Timestamp;
 }
