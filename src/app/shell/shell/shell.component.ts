fa20be7c15e51fa9bf93843e4e92296d505ed414
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-06-28 05:00:31 +0900

    feat: update

--- src/app/shell/shell/shell.component.ts
+++ src/app/shell/shell/shell.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/interfaces/user';
+import { NotificationsService } from 'src/app/services/notifications.service';
 
 @Component({
   selector: 'app-shell',
@@ -6,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shell.component.scss'],
 })
 export class ShellComponent implements OnInit {
-  constructor() {}
+  notificationCount: number;
+
+  constructor(
+    private authService: AuthService,
+    private notificationService: NotificationsService
+  ) {
+    this.authService.getUser(this.authService.uid).subscribe((user: User) => {
+      this.notificationCount = user.notificationCount;
+    });
+  }
 
   ngOnInit(): void {}
+
+  clearNotificationCount() {
+    this.notificationService.clearNotificationCount(this.authService.uid);
+  }
 }
