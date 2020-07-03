0d108f5e4f58dbaaaa02c68174cf0ebdacbc6e53
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-07-04 00:36:48 +0900

    feat: update

---
+++ src/app/upgrade/upgrade.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UpgradeRoutingModule } from './upgrade-routing.module';
+import { UpgradeComponent } from './upgrade/upgrade.component';
+
+@NgModule({
+  declarations: [UpgradeComponent],
+  imports: [CommonModule, UpgradeRoutingModule],
+})
+export class UpgradeModule {}
