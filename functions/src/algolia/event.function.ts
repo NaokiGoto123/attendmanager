67463aceb00c2845fc672da2c72c239dd27c44af
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-07-06 19:49:57 +0900

    feat(algolia): created 3 files

---
+++ functions/src/algolia/event.function.ts
@@ -0,0 +1,42 @@
+import { Algolia } from './algolia';
+import * as functions from 'firebase-functions';
+
+const algolia = new Algolia();
+
+export const createEvent = functions
+  .region('asia-northeast1')
+  .firestore.document('events/{id}')
+  .onCreate((snap) => {
+    const data = snap.data();
+    return algolia.saveRecord({
+      indexName: 'events',
+      largeConcentKey: 'body',
+      data,
+    });
+  });
+
+export const deleteEvent = functions
+  .region('asia-northeast1')
+  .firestore.document('events/{id}')
+  .onDelete((snap) => {
+    const data = snap.data();
+
+    if (data) {
+      return algolia.removeRecord('events', data.id);
+    } else {
+      return;
+    }
+  });
+
+export const updateEvent = functions
+  .region('asia-northeast1')
+  .firestore.document('events/{id}')
+  .onUpdate((change) => {
+    const data = change.after.data();
+    return algolia.saveRecord({
+      indexName: 'events',
+      largeConcentKey: 'body',
+      isUpdate: true,
+      data,
+    });
+  });
---
+++ functions/src/algolia/group.function.ts
@@ -0,0 +1,42 @@
+import { Algolia } from './algolia';
+import * as functions from 'firebase-functions';
+
+const algolia = new Algolia();
+
+export const createGroup = functions
+  .region('asia-northeast1')
+  .firestore.document('groups/{id}')
+  .onCreate((snap) => {
+    const data = snap.data();
+    return algolia.saveRecord({
+      indexName: 'groups',
+      largeConcentKey: 'body',
+      data,
+    });
+  });
+
+export const deleteGroup = functions
+  .region('asia-northeast1')
+  .firestore.document('groups/{id}')
+  .onDelete((snap) => {
+    const data = snap.data();
+
+    if (data) {
+      return algolia.removeRecord('groups', data.id);
+    } else {
+      return;
+    }
+  });
+
+export const updateGroup = functions
+  .region('asia-northeast1')
+  .firestore.document('groups/{id}')
+  .onUpdate((change) => {
+    const data = change.after.data();
+    return algolia.saveRecord({
+      indexName: 'groups',
+      largeConcentKey: 'body',
+      isUpdate: true,
+      data,
+    });
+  });
