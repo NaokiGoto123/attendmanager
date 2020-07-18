b03f019f9047ea7530484151792735259f8eeccc
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-07-18 16:32:01 +0900

    feat(events list): add infinite scroll

--- src/app/shell/shared/events-list-view/events-list-view.component.ts
+++ src/app/shell/shared/events-list-view/events-list-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Event } from 'src/app/interfaces/event';
 
 @Component({
@@ -11,7 +11,13 @@ export class EventsListViewComponent implements OnInit {
 
   @Input() existence: boolean;
 
+  @Output() more: EventEmitter<boolean> = new EventEmitter();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  moreFunction() {
+    this.more.emit(true);
+  }
 }
