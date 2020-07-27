d78dc874ff187c19dab92435973014e875fee48e
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-07-27 23:23:39 +0900

    feat(index): export

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,9 +4,14 @@ admin.initializeApp(functions.config().firebase);
 
 export * from './notifications.function';
 export * from './delete.function';
+
 export * from './algolia/event.function';
 export * from './algolia/group.function';
 export * from './algolia/user.function';
 export * from './algolia/notification.function';
+
 export * from './stripe/customer.function';
 export * from './stripe/payment-methods.function';
+export * from './stripe/intent.function';
+export * from './stripe/invoice.function';
+export * from './stripe/customer-portal.function';
