e6dc475d8d4f57563cd77e0e992db941d3bea625
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-07-28 16:09:10 +0900

    feat(payment history): create

--- src/app/shell/settings/payments-history/payments-history.component.ts
+++ src/app/shell/settings/payments-history/payments-history.component.ts
@@ -1,12 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CustomerService } from 'src/app/services/customer.service';
+import { ChargeWithInvoice } from 'src/app/interfaces/charge';
 @Component({
   selector: 'app-payments-history',
   templateUrl: './payments-history.component.html',
   styleUrls: ['./payments-history.component.scss'],
 })
 export class PaymentsHistoryComponent implements OnInit {
-  constructor() {}
+  charges: ChargeWithInvoice[];
+  startingAfter: string;
+  endingBefore: string;
+  page = 0;
+  hasNext: boolean;
+  loading: boolean;
+
+  constructor(private customerService: CustomerService) {
+    this.getCharges();
+  }
 
   ngOnInit(): void {}
+
+  getCharges(params?: { startingAfter?: string; endingBefore?: string }) {
+    this.loading = true;
+    this.customerService.getInvoices(params).then((result) => {
+      this.hasNext = !!params?.endingBefore || result?.has_more;
+      this.charges = result?.data;
+      this.loading = false;
+    });
+  }
+
+  nextPage() {
+    this.page++;
+    this.getCharges({
+      startingAfter: this.charges[this.charges.length - 1].id,
+    });
+  }
+
+  prevPage() {
+    this.page--;
+    this.getCharges({
+      endingBefore: this.charges[0].id,
+    });
+  }
 }
