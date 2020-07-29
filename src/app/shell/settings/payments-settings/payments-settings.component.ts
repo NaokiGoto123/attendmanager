c4ca3a389bafa09cb78e4ba852892f6fc28446b7
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-07-29 15:32:17 +0900

    feat(payments settings): update nav list

--- src/app/shell/settings/payments-settings/payments-settings.component.ts
+++ src/app/shell/settings/payments-settings/payments-settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ConnectedAccountService } from 'src/app/services/connected-account.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-payments-settings',
@@ -9,11 +11,23 @@ import { ActivatedRoute } from '@angular/router';
 export class PaymentsSettingsComponent implements OnInit {
   uid: string;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  connectedAccountId$: Observable<string> = this.connectedAccountService
+    .connectedAccountId$;
+
+  connectedAccountPortal = this.connectedAccountService.accountPortalUrl;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private connectedAccountService: ConnectedAccountService
+  ) {
     this.activatedRoute.queryParamMap.subscribe((params) => {
       this.uid = params.get('id');
     });
   }
 
   ngOnInit(): void {}
+
+  createStripeConnectedAccount() {
+    this.connectedAccountService.createStripeConnectedAccount();
+  }
 }
